25d94bf6c8016326b3bf45ee3e8f6323c2c2bac2
baraabilal <baraa28.ziad@gmail.com>
2020-11-30 02:16:39 +0200

    Update io.py
    remove debug lines

--- helpers/io.py
+++ helpers/io.py
@@ -4,8 +4,6 @@ def load_data(file_name='questions.csv'):
     with open(file_name, 'r') as f:
         rows = [row.split(',') for row in f.read().split('\n')]
         rows = rows[1:len(rows)]
-        print("answers are")
-        print(rows)
     return rows
 
 def write_response(response, file_name='responses.csv'):
@@ -15,7 +13,5 @@ def write_response(response, file_name='responses.csv'):
 def load_responses(file_name='responses.csv'):
     with open(file_name, 'r') as f:
         rows = [row.split(',') for row in f.read().split('\n')]
-        print("my answers are")
-        print(rows)
 
     return {row[1]: row[0] for row in rows}
